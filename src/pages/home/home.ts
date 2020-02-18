04cc3d824f5cffd42079600894296910fc2a940b
Johanna Pauna <t7pajo01@students.oamk.fi>
2020-02-18 14:09:04 +0200

    cleanup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,22 +8,9 @@ import { NavController } from 'ionic-angular';
     selector: 'page-home',
     templateUrl: 'home.html'
 })
-<<<<<<< HEAD
-export class HomePage{
-  displayData: any = { pdfSource: {url: 'assets/pdf/Johanna_Pauna_CV.pdf'}};
-
-    constructor(public navCtrl: NavController,) {
-    }
-
-    ionViewDidLoad() {
-
-
-    }
-=======
 export class HomePage {
   imageLink;
   public showButton = true;
->>>>>>> 9ecb9f689d63fd442354acdf5a4dd2f56d688bf1
 
 
 
